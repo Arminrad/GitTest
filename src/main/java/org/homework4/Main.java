6a2c03deba6e3c9631f9a1972e332fcd7b28c09f
Raha <sha.shariat87@gmail.com>
2022-01-07 16:20:04 +0330

    stringshahed

--- src/main/java/org/homework4/Main.java
+++ src/main/java/org/homework4/Main.java
@@ -7,6 +7,7 @@ import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+         String shahed;
         Connection connection = DriverManager.getConnection
                 ("jdbc:postgresql://localhost:5432/postgres", "postgres", "admin");
         Statement statement = connection.createStatement();
